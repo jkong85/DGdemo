87361f474e198c58bbd9b085631cc7db9cc98b73
Jian <kongjian.work@gmail.com>
2018-06-23 20:29:59 -0500

    add server daemon

--- services/server/src/main/java/com/kj/dg/services/server/Server.java
+++ services/server/src/main/java/com/kj/dg/services/server/Server.java
@@ -1,9 +1,6 @@
 package com.kj.dg.services.server;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -30,7 +27,7 @@ public class Server {
                 new HandlerThread(client);
             }
         } catch (Exception e) {
-            System.out.println("服务器异常: " + e.getMessage());
+            System.out.println("Server failed : " + e.getMessage());
         }
     }
 
@@ -46,32 +43,66 @@ public class Server {
             try {
                 // 读取客户端数据
                 DataInputStream input = new DataInputStream(socket.getInputStream());
-                String clientInputStr = input.readUTF();//这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
+                String clientInputStr = input.readUTF(); //这里要注意和客户端输出流的写方法对应,否则会抛 EOFException
                 // 处理客户端数据
-                System.out.println("客户端发过来的内容:" + clientInputStr);
-
+                System.out.println("CMD from client : " + clientInputStr);
+                // Manage k8s kubctl
+                String response = runShellCmd(parseCmd(clientInputStr));
                 // 向客户端回复信息
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                System.out.print("请输入:\t");
+                //System.out.print("请输入:\t");
                 // 发送键盘输入的一行
-                String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
-                out.writeUTF(s);
-
+                //String s = new BufferedReader(new InputStreamReader(System.in)).readLine();
+                //out.writeUTF(s);
+                out.writeUTF(response);
                 out.close();
                 input.close();
             } catch (Exception e) {
-                System.out.println("服务器 run 异常: " + e.getMessage());
+                System.out.println("Server run exceptionally: " + e.getMessage());
             } finally {
                 if (socket != null) {
                     try {
                         socket.close();
                     } catch (Exception e) {
                         socket = null;
-                        System.out.println("服务端 finally 异常:" + e.getMessage());
+                        System.out.println("Server finally failed: " + e.getMessage());
                     }
                 }
             }
         }
+    }
 
+    public String parseCmd(String cmd){
+        // check valid
+        return cmd;
+    }
+    public String runShellCmd(String cmd){
+        String s = null;
+        try {
+            // run the Unix "ps -ef" command
+            //Process p = Runtime.getRuntime().exec("ps -ef");
+            Process p = Runtime.getRuntime().exec(cmd);
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+            return cmd + " is done!";
+            //System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            return  cmd + " failed!";
+            //System.exit(-1);
+        }
     }
 }
